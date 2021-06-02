d8ddc72c5a3e54bc2e612a56430f625272488293
Drew-Meseck <42789729+Drew-Meseck@users.noreply.github.com>
2021-06-02 16:28:28 -0600

    Added /home routing

--- src/server.py
+++ src/server.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
+    return render_template('index.html')
+
+@app.route("/home")
+def home():
     return render_template('index.html')
 
 @app.route("/about")
 def blog():
     return render_template('about.html')
+
